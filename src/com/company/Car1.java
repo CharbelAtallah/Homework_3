6b27c8858bdc220dc36587857595aba77477f429
Charbel Atallah <chappa_Atallah@hotmail.com>
2020-10-30 11:27:23 +0100

    nya metoder, omformaterat, fixat till Main

--- src/com/company/Car1.java
+++ src/com/company/Car1.java
@@ -1,34 +1,51 @@
 package com.company;
 
-public class Car1 {
-    int yearModel;
-    String make;
-    int speed;
+import javax.swing.*;
 
-    public int getYearModel() {
+public class Car1 {
+    private static int speed; //Varför private?
+    private static int yearModel;
+    private static String make;
+    //int yearModel;
+    //String make;
+    //int speed;
+
+    public static int getYearModel() {
         return yearModel;
     }
 
-    public String getMake(){
+    public static String getMake() {
         return make;
     }
 
-    public void accelerate(){
-        speed += 5;
-    }
-    public void brake(){
-        speed -= 5;
+
+    public static void accelerate() {
+        for (int seconds = 1; seconds <= 3; seconds++) { //"3" bestämmer hur många loopar det är
+            speed += 20; //Bestämmer intervallen på hastigheten t.ex 20, 40, 60
+            System.out.println("The current speed of the " + getYearModel() + " " + getMake() + " is: " + speed + " KM/H");
+        }
     }
 
-    public void speed(int maxSpeed) {
-        System.out.println("The car can go faster than 25!");
-        System.out.println("Max speed is: " + maxSpeed + ". Keep going!!");
+    public void speed(String maxSpeed) {
+        System.out.println("Max speed is: " + maxSpeed + ". Keep going! The car gan go faster"); //IKNTEGRERA
     }
 
-    public Car1 (int yearModelGiven, String makeGiven) {
+    public static void brake() {
+        for (int seconds = 1; seconds <= 3; seconds++) {
+            speed -= 20;
+            System.out.println("The current speed of the " + getYearModel() + " " + getMake() + " is: " + speed + " KM/H");
+
+            if (speed == 0) {
+                System.out.println("The car has halted to a stop!"); //Så att man inte får en utskrift i varje rad.
+            }
+        }
+    }
+
+    public Car1(int yearModelGiven, String makeGiven) {
         yearModel = yearModelGiven;
         make = makeGiven;
-        speed=0;
+        speed = 0;
     }
+
 
 }
--- src/com/company/Car1Demo.java
+++ src/com/company/Car1Demo.java
@@ -1,30 +1,21 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Car1Demo {
     public static void main(String[] args) {
 
         //Mfix something that makes the user put in.-Terminal or dialogue 
         Car1 car1 = new Car1(2017, "Audi");
 
+        String mxspd = JOptionPane.showInputDialog("whats the max speed?");
+        car1.speed(mxspd);
 
-        for (int seconds = 1; seconds <= 5; seconds++) {
-            car1.accelerate();
-            System.out.println("The current speed of the " + car1.getYearModel() + " " + car1.getMake() + " is: " + car1.speed + " KM/H");
-        }
+        Car1.accelerate();
 
-
-        car1.speed(200);
         System.out.println("No wait... start breaking!");
 
-        for (int seconds = 1; seconds <= 5; seconds++) {
-            car1.brake();
-            System.out.println("The current speed of the " + car1.getYearModel() + " " + car1.getMake() + " is: " + car1.speed + " KM/H");
-
-            if (car1.speed == 0) {
-                System.out.println("The car has halted to a stop!");
-            }
-        }
-
+        Car1.brake();
 
     }
 }
